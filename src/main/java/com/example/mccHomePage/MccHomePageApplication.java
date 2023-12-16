8e3801e08836351101f5f03d39af35d7831d9e97
hyunseong <rlagustjd2684@gmail.com>
2023-12-16 13:28:07 +0900

    백엔드 프로젝트 초기 설정

---
+++ src/main/java/com/example/mccHomePage/MccHomePageApplication.java
@@ -0,0 +1,13 @@
+package com.example.mccHomePage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MccHomePageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MccHomePageApplication.class, args);
+	}
+
+}
