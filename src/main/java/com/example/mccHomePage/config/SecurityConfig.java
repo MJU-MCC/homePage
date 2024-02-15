6f360ac5d74e282df4c18b01bf21fee17de13e96
Hyun Seong Kim <rlagustjd2684@gmail.com>
2024-02-15 16:50:26 +0900

    Security 설정 추가

---
+++ src/main/java/com/example/mccHomePage/config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.example.mccHomePage.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+
+        return http
+                .csrf().disable()
+                .cors()
+                .and()
+
+                .httpBasic().disable()
+                .formLogin().disable()
+
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+
+                .addFilterBefore()
+                .authorizeRequests()
+                .anyRequest().permitAll()
+                .build();
+
+
+    }
+}
