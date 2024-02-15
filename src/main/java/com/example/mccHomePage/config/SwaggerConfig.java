288e1aa109b1f9ee5a851ff976fc3aa9cc64e345
Hyun Seong Kim <rlagustjd2684@gmail.com>
2024-02-15 15:34:17 +0900

    [feat] 현성 : 스웨거 설정 정보  추가

---
+++ src/main/java/com/example/mccHomePage/config/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package com.example.mccHomePage.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.ant("/member/**"))
+                .build()
+                .apiInfo(info());
+    }
+
+    private ApiInfo info(){
+        return
+                new ApiInfoBuilder()
+                        .title("동아리 홈페이지 제작 api 문서")
+                        .description("MCC")
+                        .version("1.0.0")
+                        .build();
+    }
+}
